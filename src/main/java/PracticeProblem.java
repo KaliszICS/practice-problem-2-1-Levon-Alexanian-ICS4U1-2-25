188d67ec882fbdd1b59d7134b90bdec53ea59185
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 05:51:42 +0000

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,34 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
-	}
-
-	
-
+    public static int find(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int findLast(String[] array, String string) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i].equals(string)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int findSecond(char[] array, char character) {
+        int count = 0;
+        int firstIndex = -1; 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == character) {
+                count++;
+                if (count == 1) {
+                    firstIndex = i;
+                } else if (count == 2) {
+                    return i;
+                }
+            }
+        }
+        return firstIndex;
+    }
+	// CHICKEN JOCKEY, I AM PLACING BLOCKS AND STUFF BECAUSE THIS IS FREAKING MINECRAFT, FLINT AN' STEEL
 }
